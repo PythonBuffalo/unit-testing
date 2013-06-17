d893c1321197ab876ffc6eac4951113f20cb3bcb
brettlangdon <brett@blangdon.com>
2013-06-17 07:50:34 -0400

    show assertions on parent mock object not through method

--- 3_mock/2_assertions.py
+++ 3_mock/2_assertions.py
@@ -1,30 +1,30 @@
 import mock
 
 mocked_obj = mock.Mock()
-mocked_obj.some_method()
+mocked_obj()
 
-print 'some_method was called: %r' % (mocked_obj.some_method.called, )
+print 'some_method was called: %r' % (mocked_obj.called, )
 
 # reset the state of called
 mocked_obj.reset_mock()
-print 'some_method was called: %r' % (mocked_obj.some_method.called, )
+print 'some_method was called: %r' % (mocked_obj.called, )
 
 # call mocked method with arguments
-mocked_obj.some_method('here', 'are', some='arguments')
-mocked_obj.some_method.assert_called_with('here', 'are', some='arguments')
+mocked_obj('here', 'are', some='arguments')
+mocked_obj.assert_called_with('here', 'are', some='arguments')
 
 # get the arguments that the method was called with
-print 'some_method was called with %r' % (mocked_obj.some_method.call_args, )
+print 'some_method was called with %r' % (mocked_obj.call_args, )
 
 # invalid argument assertion throws the proper exception
 try:
-    mocked_obj.some_method.assert_called_with('other' 'arguments')
+    mocked_obj.assert_called_with('other' 'arguments')
 except AssertionError:
     print 'some_method was not called with those arguments'
 
 # get the number of times the method was called
-mocked_obj.some_method('other', 'arguments')
-print 'some_method has been called %s times' % (mocked_obj.some_method.call_count, )
+mocked_obj('other', 'arguments')
+print 'some_method has been called %s times' % (mocked_obj.call_count, )
 
 # get all the method calls to the mock object
 mocked_obj.other_method()
